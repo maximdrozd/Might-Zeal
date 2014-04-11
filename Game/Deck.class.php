a09fb4ab2fa3eaa3c9456400b1ec39b75f0c91b5
Max Drozd <maxim.drozd@gmail.com>
2014-04-10 21:15:44 -0400

    Fixed Deck bug

--- Game/Deck.class.php
+++ Game/Deck.class.php
@@ -8,9 +8,7 @@ class Deck extends CardCollection {
 
 	public function draw(){
 		if($this->size() > 0){
-			$card = $this->cards[0];
-			$this->pop($card);
-			return $card;
+			return $this->remove($this->cards[0]);
 		}
 		return null;
 	}
